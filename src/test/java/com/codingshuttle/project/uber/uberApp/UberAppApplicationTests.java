125d990bdf0e63e46fa0e4979397fa5cdb18e10e
JatinBawne <jatinbawne2347@gmail.com>
2025-01-15 00:59:59 +0530

    Email Service added

--- src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,32 @@
 package com.codingshuttle.project.uber.uberApp;
 
+import com.codingshuttle.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail("pelisif969@suggets.com",
+				"This is the testing email",
+						"This is body my my mail");
+	}
+
+	@Test
+	void sendEmailMultiple(){
+		String email[] = {
+				"pelisif969@suggets.com",
+				"jatinbawne2347@gmail.com",
+				"nidhibawne2000@gmail.com"
+		};
+
+		emailSenderService.sendEmail(email, "This is mail Subject, J", "This is mail Body J");
 	}
 
 }
