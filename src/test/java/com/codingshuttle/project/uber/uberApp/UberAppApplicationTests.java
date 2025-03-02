6501961c2fc1e8538c0aa2d0b2a9492f8693adbc
JatinBawne <jatinbawne2347@gmail.com>
2025-03-02 23:20:28 +0530

    prod application properties

--- src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
@@ -8,25 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UberAppApplicationTests {
 
-	@Autowired
-	private EmailSenderService emailSenderService;
 
-	@Test
-	void contextLoads() {
-		emailSenderService.sendEmail("pelisif969@suggets.com",
-				"This is the testing email",
-						"This is body my my mail");
-	}
-
-	@Test
-	void sendEmailMultiple(){
-		String email[] = {
-				"pelisif969@suggets.com",
-				"jatinbawne2347@gmail.com",
-				"nidhibawne2000@gmail.com"
-		};
-
-		emailSenderService.sendEmail(email, "This is mail Subject, J", "This is mail Body J");
-	}
 
 }
